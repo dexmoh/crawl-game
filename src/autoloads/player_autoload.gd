c2939000c851b2b4b0c5899676b2ecb45c5149d5
dexmoh <lazar.petrovic2310@gmail.com>
2026-03-01 16:22:42 +0100

    Remove inventory.

--- src/autoloads/player_autoload.gd
+++ src/autoloads/player_autoload.gd
@@ -3,8 +3,6 @@
 extends Node
 
 var body: PlayerController
-var inventory: Inventory
 
 func _ready():
 	body = get_tree().get_first_node_in_group("player") as PlayerController
-	inventory = Inventory.new()
